7b2f447b9f63cef73714941170a07da951d868c1
xinzezhang <xinze@hust.edu.cn>
2020-08-04 04:00:18 +0800

    update

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 cd ./wmt17_systems/en-zh
 
-src_file=/Users/xinze/Documents/GitHub/nematus/wmt17_systems/wmt17.datasets.pair.enzh/test/newstest2017-enzh-src.en
+# src_file=../wmt17.datasets.pair.enzh/test/newstest2017-enzh-src.en.test
+src_file=../wmt17.datasets.pair.enzh/test/newstest2017-enzh-src.en
 
-output_file=/Users/xinze/Documents/GitHub/nematus/wmt17_systems/wmt17.datasets.pair.enzh/output/newstest2017-enzh-output.zh
+output_file=../wmt17.datasets.pair.enzh/output/newstest2017-enzh-output.zh
 
 ./tf-translate-single.sh < $src_file > $output_file
